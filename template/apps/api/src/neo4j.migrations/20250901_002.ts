cf6b386421f5db854f656997f2ebb93ebf0d79b8
Carlo Nicora <carlo.nicora@gmail.com>
2025-12-18 22:13:03 +0100

    fix: update featureId in migration queries to ensure proper access control

--- template/apps/api/src/neo4j.migrations/20250901_002.ts
+++ template/apps/api/src/neo4j.migrations/20250901_002.ts
@@ -11,7 +11,7 @@ export const migration: MigrationInterface[] = [
     queryParams: {
       moduleName: "Auth",
       moduleId: "035fe8a6-d467-40c0-9d1d-6a87f0dd286e",
-      featureId: null,
+      featureId: "17036fb0-060b-4c83-a617-f32259819783",
       isCore: true,
       permissions: JSON.stringify([
         { type: Action.Create, value: true },
@@ -26,7 +26,7 @@ export const migration: MigrationInterface[] = [
     queryParams: {
       moduleName: "Company",
       moduleId: "f9e77c8f-bfd1-4fd4-80b0-e1d891ab7113",
-      featureId: null,
+      featureId: "17036fb0-060b-4c83-a617-f32259819783",
       isCore: true,
       permissions: JSON.stringify([{ type: Action.Read, value: true }]),
     },
@@ -36,7 +36,7 @@ export const migration: MigrationInterface[] = [
     queryParams: {
       moduleName: "Feature",
       moduleId: "025fdd23-2803-4360-9fd9-eaa3612c2e23",
-      featureId: null,
+      featureId: "17036fb0-060b-4c83-a617-f32259819783",
       isCore: true,
       permissions: JSON.stringify([{ type: Action.Read, value: true }]),
     },
@@ -46,7 +46,7 @@ export const migration: MigrationInterface[] = [
     queryParams: {
       moduleName: "Notification",
       moduleId: "9259d704-c670-4e77-a3a1-a728ffc5be3d",
-      featureId: null,
+      featureId: "17036fb0-060b-4c83-a617-f32259819783",
       isCore: true,
       permissions: JSON.stringify([
         { type: Action.Read, value: true },
@@ -59,7 +59,7 @@ export const migration: MigrationInterface[] = [
     queryParams: {
       moduleName: "Role",
       moduleId: "9f6416e6-7b9b-4e1a-a99f-833191eca8a9",
-      featureId: null,
+      featureId: "17036fb0-060b-4c83-a617-f32259819783",
       isCore: true,
       permissions: JSON.stringify([{ type: Action.Read, value: true }]),
     },
@@ -69,7 +69,7 @@ export const migration: MigrationInterface[] = [
     queryParams: {
       moduleName: "S3",
       moduleId: "db41ba46-e171-4324-8845-99353eba8568",
-      featureId: null,
+      featureId: "17036fb0-060b-4c83-a617-f32259819783",
       isCore: true,
       permissions: JSON.stringify([{ type: Action.Read, value: true }]),
     },
@@ -79,7 +79,7 @@ export const migration: MigrationInterface[] = [
     queryParams: {
       moduleName: "User",
       moduleId: "04cfc677-0fd2-4f5e-adf4-2483a00c0277",
-      featureId: null,
+      featureId: "17036fb0-060b-4c83-a617-f32259819783",
       isCore: true,
       permissions: JSON.stringify([
         { type: Action.Read, value: true },
